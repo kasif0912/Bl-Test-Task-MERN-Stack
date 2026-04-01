bf289d9ca90e056e327ace8cca40e7cee6fe1ae6
kasif Ali <kasifali009@gmail.com>
2026-04-01 13:35:35 +0530

    added port

--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("hello server :)");
