62950f00701997c92400694396939bc56c2dcae1
kasif Ali <kasifali009@gmail.com>
2026-04-01 16:07:42 +0530

    email setup instructions

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://bl-test-task-mern-stack-1.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
 });
 
 api.interceptors.request.use((config) => {
--- server/utils/sendInviteEmail.js
+++ server/utils/sendInviteEmail.js
@@ -14,10 +14,26 @@ const sendInviteEmail = async (to, inviteLink) => {
     to,
     subject: "You're invited to join a group",
     html: `
-      <h3>You have been invited to join a group</h3>
-      <p>Click the link below to join:</p>
-      <a href="${inviteLink}">${inviteLink}</a>
-      <p>This link will expire in 7 days.</p>
+      <h2>You're Invited 🎉</h2>
+      
+      <p>You have been invited to join a group on Expense Split.</p>
+      
+      <p><strong>To get started:</strong></p>
+      <ol>
+        <li>Click the button below</li>
+        <li>Register your account using the same email</li>
+        <li>You will automatically join the group after signup</li>
+      </ol>
+
+      <a href="${inviteLink}" 
+         style="display:inline-block;padding:10px 16px;background:#4F46E5;color:white;text-decoration:none;border-radius:6px;">
+         Sign Up & Join Group
+      </a>
+
+      <p style="margin-top:10px;">Or copy this link:</p>
+      <p>${inviteLink}</p>
+
+      <p><strong>Note:</strong> This invitation link will expire in 7 days.</p>
     `,
   };
 
